83769fa02e2b7d2f8a91c0a28c71894f1c025616
ratero <aa.ratero@gmail.com>
2019-09-21 12:24:11 -0300

    add social media shared float

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+// Carousel
 $(document).ready(function () {
     // Ativa o Carousel
     $('.carousel').carousel({
         interval: 5000
     });
 
+});
+
+// Navbar Hide Navbar on Scroll Down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    } else {
+    document.getElementById("navbar").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+// jquery-social-share-bar
+$('#share-bar').share({
+    position: 'right'
 });
